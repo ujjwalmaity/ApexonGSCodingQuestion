361dd20f5de7f19e2c59d9b716dc8b768d5d3ad6
Ujjwal Maity <ujjwalmaity98@gmail.com>
2024-10-18 13:21:00 +0530

    Convert a String "aabbb" to "a2b3"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 
 public class Main {
     public static void main(String[] args) {
+        // Convert a String "aabbb" to "a2b3"
+        Q1ConvertAString q1 = new Q1ConvertAString();
+        q1.call("aabbb");
     }
 }
---
+++ src/Q1ConvertAString.java
@@ -0,0 +1,32 @@
+
+/// Convert a string as given in the format below:
+/// Example:
+/// aabbb into a2b3
+/// aaaa into a4
+/// a into a1
+
+public class Q1ConvertAString {
+    public void call(String text) {
+        StringBuilder str = new StringBuilder();
+
+        char lastChar = text.charAt(0);
+        int count = 1;
+
+        for (int i = 1; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            if (lastChar == ch) {
+                count++;
+            } else {
+                str.append(lastChar);
+                str.append(count);
+                lastChar = ch;
+                count = 1;
+            }
+        }
+
+        str.append(lastChar);
+        str.append(count);
+
+        System.out.println(str);
+    }
+}
