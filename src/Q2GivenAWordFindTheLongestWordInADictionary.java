416ebf2201b3a95ca2196c999a684c403073d843
Ujjwal Maity <ujjwalmaity98@gmail.com>
2024-10-18 14:29:15 +0530

    Given A Word Find The Longest Word In A Dictionary

---
+++ src/Q2GivenAWordFindTheLongestWordInADictionary.java
@@ -0,0 +1,28 @@
+public class Q2GivenAWordFindTheLongestWordInADictionary {
+    public void call() {
+        // Given a dictionary (list of words) and a substring,
+        // you have to return the length of the longest word in the dictinary containing the substring.
+        //
+        // Example:
+        // String[] dict = {"CODGE", "ODG", "LODGES", "SODG", "dodge", "mODJ", "LODGESSSS"}
+        // String toSearch = "ODG";
+        //
+        // Returns 9 (LODGESSSS)
+
+        String[] dict = {"CODGE", "ODG", "LODGES", "SODG", "dodge", "mODJ", "LODGESSSS"};
+        String toSearch = "ODG";
+
+        String res = "";
+        int resLength = 0;
+
+        for (String str : dict) {
+            int strLength = str.length();
+            if (str.contains(toSearch) && strLength >= resLength) {
+                res = str;
+                resLength = strLength;
+            }
+        }
+
+        System.out.println(res);
+    }
+}
