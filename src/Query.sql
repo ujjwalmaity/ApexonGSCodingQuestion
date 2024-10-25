41ce04e08186bdce1a0bd8c0159b0ec102b5a090
Ujjwal Maity <ujjwalmaity98@gmail.com>
2024-10-25 10:04:40 +0530

    MySql Query

--- src/Query.sql
+++ src/Query.sql
@@ -6,7 +6,17 @@ select * from Employees;
 select * from Projects;
 select * from EmployeeProjects;
 
+select * from Employees
+order by salary desc
+limit 5;
 
+select * from Employees
+order by salary ASC
+limit 5 offset 10;
+
+select department_id, SUM(salary) as total_salary from Employees
+group by department_id
+having total_salary > 30000;
 
 # truncate table Departments;
 # drop table Departments;
