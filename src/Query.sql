984aa8dafed15ac241ce3ef1c1eb2c26ff72e206
Ujjwal Maity <ujjwalmaity98@gmail.com>
2024-10-25 10:19:17 +0530

    MySql Query

--- src/Query.sql
+++ src/Query.sql
@@ -18,5 +18,12 @@ select department_id, SUM(salary) as total_salary from Employees
 group by department_id
 having total_salary > 30000;
 
+SELECT employee_id, salary,
+    CASE 
+        WHEN salary > 7000 THEN 'Above $7000'
+        ELSE 'Below or Equal to $7000'
+    END AS salary_note
+FROM employees;
+
 # truncate table Departments;
 # drop table Departments;
