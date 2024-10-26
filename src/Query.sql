4369b9c889b9764e0d1d2f0e6b1c1f0f6f55eedc
Ujjwal Maity <ujjwalmaity98@gmail.com>
2024-10-26 14:06:40 +0530

    MySql Query

--- src/Query.sql
+++ src/Query.sql
@@ -16,7 +16,7 @@ limit 5 offset 10;
 
 select department_id, SUM(salary) as total_salary from Employees
 group by department_id
-having total_salary > 30000;
+having total_salary > 15000;
 
 select id, salary,
     case
@@ -53,6 +53,85 @@ group by e.department_id;
 select id, title, sum(hours_worked) as total_hours_worked from EmployeeProjects
 join Projects on project_id = id
 group by project_id;
+
+select * from Employees
+where salary > (select avg(salary) from Employees);
+
+select id, title, date_format(start_date,'%M %d, %Y') as formatted_start_date from Projects;
+
+# select * from Employees e
+# select e.id, e.first_name, e.last_name, d.name as department_name, p.title as project_title from Employees e
+select e.id, concat(e.first_name, ' ', e.last_name) as full_name, d.name as department_name, p.title as project_title from Employees e
+join Departments d on e.department_id = d.id
+left join EmployeeProjects ep on e.id = ep.employee_id
+left join Projects p on ep.project_id = p.id
+order by e.id asc;
+
+# select * from Projects p
+select p.id, p.title,
+	case
+		# when ep.project_id then 'Assigned'
+		# when p.id = ep.project_id then 'Assigned'
+        when ep.project_id is not null then 'Assigned'
+        else 'Unassigned'
+    end as status
+from Projects p
+left join EmployeeProjects ep on p.id = ep.project_id
+group by p.id;
+
+select e.department_id, d.name as department_name, round(avg(e.salary), 2) as average_salary from Employees e
+join Departments d on e.department_id = d.id
+group by e.department_id
+having count(e.id) > 1;
+
+select id, concat(first_name, ' ', last_name) as full_name from Employees;
+
+select * from Employees e
+join Departments d on e.department_id = d.id
+where e.salary > 5000 and d.name = 'IT';
+
+update Employees e
+join Departments d on e.department_id = d.id
+set e.salary = 7000
+where e.salary > 6500 and d.name = 'IT';
+
+select * from Employees
+where department_id = 
+	(select id from Departments where name = 'HR')
+and salary = 
+	(select max(salary) from Employees e
+    join Departments d on e.department_id = d.id
+    where name = 'HR');
+
+select * from Employees e
+join Departments d on e.department_id = d.id
+where d.name = 'HR' and e.salary = 
+	(select max(salary) from Employees e
+	join Departments d on e.department_id = d.id
+	where d.name = 'HR');
+
+select * from Employees e
+join Departments d on e.department_id = d.id
+where d.name = 'HR'
+order by salary desc
+limit 1;
+
+select * from Employees e
+join EmployeeProjects ep on e.id = ep.employee_id
+join Projects p on ep.project_id = p.id
+# where p.end_date between current_date and (current_date + interval 2 month);
+where p.end_date between curdate() and date_add(curdate(), interval 2 month);
+
+select p.id as project_id, p.title as project_title, p.budget as project_budget, 
+       sum(e.salary * ep.hours_worked) as total_billed,
+       # row_number() over(order by project_id asc) as line_number
+       # row_number() over(order by project_id) as line_number
+       row_number() over() as line_number
+from Projects p
+join EmployeeProjects ep on p.id = ep.project_id
+join Employees e on ep.employee_id = e.id
+group by p.id
+having total_billed > 10000;
 
 # truncate table EmployeeProjects;
 # drop table EmployeeProjects;
